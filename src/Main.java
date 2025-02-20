334efedf1b545242707a558351f04cd8fb4e83d9
jasper <whiteana001@warren.k12.in.us>
2025-02-20 13:46:20 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Main {
+    public static void main(String[] args) {
+        Student alice = new Student("Alice", 4.0);
+        Student jasper = new Student("Jasper", 3.7);
+        Student jay = new Student("Jay", 1.7);
+
+        List<Student> school = new ArrayList<>();
+        school.add(alice);
+        school.add(jasper);
+        school.add(jay);
+
+        Collections.sort(school);
+        System.out.print(school);
+    }
+
+
+
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
 
-public class Student {
+public class Student implements Comparable<Student> {
+    private String name;
+    private double gpa;
+
+
+    public Student(String name, double gpa){
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString(){
+        return name + " GPA: " + gpa;
+    }
+
+    @Override
+    public int compareTo(Student other){
+        return Double.compare(this.gpa, other.gpa);
+    }
+
 }
